b4f503ec7afc1c55fe0211872bb10fb82b213dfe
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-08 16:30:17 +0000

    Fri, Jul 8, 2022, 4:30 PM +00:00

---
+++ population/population.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start s
+    int s = get_int("give start size");
+    if s<9
+    {
+        int s = get_int("give start size");
+        }
+    else
+    {
+        int e = get_int("give end size ");
+    if e<s
+    {
+        int e = get_int("give end size ");
+    }
+    else
+    {
+        int n = 0;
+        while (e>=s)
+        s=s+s/3-s/4;
+        n=n+1;
+        printf("years:"n);
+
+
+    }
+    }
+}
+
+
+
+
