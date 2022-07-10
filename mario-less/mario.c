66a5aa343f1e8c5705d593e12e063a1a6f0f4651
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-10 12:45:53 +0000

    Sun, Jul 10, 2022, 12:45 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int n;
+    do
+    {
+        n = get_int("height:");
+    }
+    while (n < 1 ||	n>8 );
+
+    // For each row
+    for (int i = 0; i < n; i++)
+    {
+        // For each column
+        for (int j = 0; j < i+1; j++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
 }
