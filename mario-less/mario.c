7ced7f9330b70cc36e8693963ee3c8f6e44cf5d2
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-10 13:07:39 +0000

    Sun, Jul 10, 2022, 1:07 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -14,7 +14,7 @@ int main(void)
     for (int i = 0; i < n; i++)
     {
         // For each column
-        for (int k = 0; k < n-i; k++)
+        for (int k = 0; k < n-i-1; k++)
             printf(".");
         for (int j = 0; j < i+1; j++)
             printf("#");
