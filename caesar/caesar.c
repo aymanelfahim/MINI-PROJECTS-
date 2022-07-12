2d39ef497841a201257470385fdad2b9aa7a0a1a
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-12 22:22:19 +0000

    Tue, Jul 12, 2022, 10:22 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,51 +6,47 @@
 
 int main(int argc, string argv[])
 {
-    for (int i = 0, n = strlen(argv); i < n; i++)
-    {
-        if (argc == 2 && isdigit(argv[i]))
+if  (argc != 2)
+{
+ printf(" chi qlwa");
+ return 1;
+}
+
+    for (int i = 0; i < strlen(argv[1]); i++)
+{
+        if (!isdigit(argv[1][i]))
         {
-        int atoi(string argv[1]);
-        argv[1]=k
-         a=get_string("plaint text:")
+        printf(" chi qlawi");
+        return 2;
+        }
+}
+
+        key=atoi(argv[1])
+
+        a=get_string("plaint text:")
         for (int j = 0, n = strlen(a); j < n; j++)
 {
-        if (is upper(a[j]))
+        if (isupper(a[j]))
         {
-        a[j]=a[j]-65;
+        a[j]=(a[j]-65+key)%26;
+        a[j]=a[j]+65
         }
-        if ( is lower(a[j]))
-        a[j]=a[j]-91;
-}
-        for (int g = 0, n = strlen(a); g < n; g++)
+        if ( islower(a[j]))
         {
-        a[g]=(a[g]+k)%26;
-        }
-        for (int l = 0, n = strlen(a); l < n; l++)
-        {
-        if (is lower(a[l]) )
-        { a[l]=a[l]+91
-
-        }
-        if (is upper(a[l]))
-        {
-            a[l]=a[l]+65
-
+        a[j]=(a[j]-91+key)%26;
+        a[j]=a[j]+91
         }
 
 
 
-        printf("ciphertext%s"a );
+
+
+
+
+        printf("ciphertext: %s",a );
     }
     }
-
-    else
-    {
-        printf("Usage: ./caesar key\n");
-        return 1;
-
-    }
-    }
+        }
 
 
 
@@ -60,4 +56,5 @@ int main(int argc, string argv[])
 
 
 
-}
+
+
