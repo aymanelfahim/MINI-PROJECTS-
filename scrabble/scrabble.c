8eab65e9524356340b20c0c3afe1807f75803b79
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-12 10:44:47 +0000

    Tue, Jul 12, 2022, 10:44 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -44,9 +44,10 @@ int compute_score(string word)
 {
     for (int i = 0 ;i < 26; i++)
     {
-    POINTS [i] = word [i+65];
-    if (islower(word [i]))
+        if (islower(word [i]))
     int toupper(char word);
+    POINTS [i] = word [i+65];
+
     }
 
 
