2563c04ba5fe57405737d5fde2c4d72783542f02
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-12 17:42:55 +0000

    Tue, Jul 12, 2022, 5:42 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, string argv[])
 {
     for (int i = 0, n = strlen(argv); i < n; i++)
     {
         if (argc = 2 && isdigit(argv[i]))
+        int atoi(string argv);
     {
         a=get_string("plaint text:")
 
