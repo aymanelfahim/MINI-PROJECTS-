89bc8c07918e2ea65d04a89409f2b6eaa180fab0
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-12 16:24:24 +0000

    Tue, Jul 12, 2022, 4:24 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -3,8 +3,8 @@
 #include <ctype.h>
 
 int main(int argc, string argv[])
-{
-    if (argc = 2 && isdigit(argv[2]))
+{ char c = argv[2]
+    if (argc = 2 && isdigit(c))
     {
         a=get_string("plaint text:")
 
