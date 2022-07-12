e78b5fb70f920d0f072ee1762ec7c805365d8fcb
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-12 16:02:50 +0000

    Tue, Jul 12, 2022, 4:02 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 int main(int argc, string argv[])
-{if (argc != 2)
-    {
-        printf("missing command-line argument\n");
-        return 1;
-    }
-    printf("hello, %s\n", argv[1]);
-    return 0;
+{
+    a=get_string("plaint text:")
+
+
+
+
+
+    printf("ciphertext%s" )
 
 
 }
