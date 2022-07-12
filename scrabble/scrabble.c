f4895fe7473aa775cadda87e3319e9abfd3325ee
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-12 12:46:09 +0000

    Tue, Jul 12, 2022, 12:46 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -45,8 +45,18 @@ int compute_score(string word)
     int score= 0;
 
     for (int i = 0 ;i < strlen(word); i++)
-    if 
-    score = score + POINTS[word [i]-65];
+    {
+        if (islower(word[i]))
+        {
+        score = score + POINTS[word [i]-65];
+
+        }
+    else if (isupper(word(i)))
+    {
+         score = score + POINTS[word [i]-91];
+    }
+
+    }
 
 
 
