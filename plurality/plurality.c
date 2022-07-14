4bd595b598998b070793a1e90ced66e56bd581be
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-14 15:15:49 +0000

    Thu, Jul 14, 2022, 3:15 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -66,16 +66,12 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
+    for (int j = 0;  ;j < candidate_count ; j++)
+    {
 while (candidates[j]!=candidates[j+1])
 
 {
 
-for (int j = 0;  ;j < candidate_count ; j++)
-{
-
-
-
-    {
         if (strcmp(candidates[j].name,name ) == 0)
         {
             candidates.votes[j]=candidates.votes[j]+1
