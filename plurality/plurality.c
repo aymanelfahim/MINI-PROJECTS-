5c0336fc4a2e35695e89c2af6d7352070503da4d
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-14 15:51:57 +0000

    Thu, Jul 14, 2022, 3:51 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -75,20 +75,23 @@ bool vote(string name)
         candidates[j].votes=candidates[j].votes+1;
         }
 
+return true;
 
     }
 
-
+    }
     return false;
 }
-}
+
+
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
     int winner = 0;
+    string win = 0;
     for (int g = 0; g < candidate_count ; g++)
     {
-        if winner<candidates[g].votes
+        if (winner<candidates[g].votes)
         {
             win = candidates[g].name;
         }
