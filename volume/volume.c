e6fd1b687e63398a41ecebbea272310e38a3e175
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-19 13:21:23 +0000

    Tue, Jul 19, 2022, 1:21 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,6 +34,7 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    typedef uint16_t BYTE;
         FILE *file = fopen(argv[1], "r");
     // TODO: Read samples from input file and write updated data to output file
 
