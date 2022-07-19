71423bea27e91891d6b755aac964c09622127302
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-19 17:52:38 +0000

    Tue, Jul 19, 2022, 5:52 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -40,6 +40,7 @@ int main(int argc, char *argv[])
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
     fread(&buffer sizeof(HEADER_SIZE), 1, input);
+    while 
 
 
     // Close files
