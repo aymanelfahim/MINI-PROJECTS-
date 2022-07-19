d97af49e83188a740a70c26971f40f997359735d
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-19 17:48:11 +0000

    Tue, Jul 19, 2022, 5:48 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,9 +34,13 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    typedef uint8_t 44 BYTE;
-        FILE *file = fopen(a
+    uint8_t header[HEADER_SIZE];
+        fread(header, sizeof(HEADER_SIZE), 1, input);
+        fwrite(header sizeof(HEADER_SIZE), 1, output);
     // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    fread(header sizeof(HEADER_SIZE), 1, input);
+
 
     // Close files
     fclose(input);
