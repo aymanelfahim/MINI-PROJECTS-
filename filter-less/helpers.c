36d8060e0f00abb4fe874d20b83d04b410617c52
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-20 11:43:47 +0000

    Wed, Jul 20, 2022, 11:43 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -9,6 +9,16 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j].rgbtBlue = 0x00;
+            image[i][j].rgbtRed = 0x00;
+            image[i][j].rgbtGreen = 0x00;
+
+        }
+    }
     return;
 }
 
