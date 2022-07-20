5e14649728c1cedc905632e33dcaa9490ef179ac
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-20 16:29:46 +0000

    Wed, Jul 20, 2022, 4:29 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,12 +3,6 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
-}
-
-// Convert image to sepia
-void sepia(int height, int width, RGBTRIPLE image[height][width])
-{
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
@@ -22,6 +16,13 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         image[i][j].rgbtGreen = s;
         }
     }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+
     return;
 }
 
