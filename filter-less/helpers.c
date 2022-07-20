4e9c26e1d2544b47de8c9f42ff7b69cbe93e868b
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-20 16:02:04 +0000

    Wed, Jul 20, 2022, 4:02 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -13,7 +13,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j].rgbtBlue = 0x00;
+            int k = image[i][j].rgbtBlue ;
             image[i][j].rgbtRed = 0x00;
             image[i][j].rgbtGreen = 0x00;
 
