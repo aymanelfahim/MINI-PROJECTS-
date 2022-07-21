0cfe6a9dcc421ce97f6f08596f2afce7ac3448a4
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-21 10:27:24 +0000

    Thu, Jul 21, 2022, 10:27 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -24,14 +24,30 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-    image[i][j].rgbtRed=round(image[i][j].rgbtRed*0.393+image[i][j].rgbtGreen*0.769+image[i][j].rgbtBlue*0,189)
-    image[i][j].rgbtRed=round(image[i][j].rgbtRed*0.349+image[i][j].rgbtGreen*0.769+image[i][j].rgbtBlue*0,189)
-    image[i][j].rgbtRed=round(image[i][j].rgbtRed*0.393+image[i][j].rgbtGreen*0.769+image[i][j].rgbtBlue*0,189)
+    image[i][j].rgbtRed=round(image[i][j].rgbtRed*0.393+image[i][j].rgbtGreen*0.769+image[i][j].rgbtBlue*0.189);
+    image[i][j].rgbtGreen=round(image[i][j].rgbtRed*0.349+image[i][j].rgbtGreen*0.686+image[i][j].rgbtBlue*0.168);
+    image[i][j].rgbtBlue=round(image[i][j].rgbtRed*0.272+image[i][j].rgbtGreen*0.534+image[i][j].rgbtBlue*0.131);
+    if (image[i][j].rgbtRed>255)
+{
+    image[i][j].rgbtRed=255;
+}
+if (image[i][j].rgbtGreen>255)
+{
+     image[i][j].rgbtGreen=255;
+}
+if (image[i][j].rgbtBlue>255)
+{
+     image[i][j].rgbtBlue=255;
+}
+
 
 
     return;
 }
+
     }
+
+
 }
 
 // Reflect image horizontally
