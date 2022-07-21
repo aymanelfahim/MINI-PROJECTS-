c0920f77483509833db031c823d1f44780232543
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-21 10:48:36 +0000

    Thu, Jul 21, 2022, 10:48 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -20,30 +20,19 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    int limit(int rgb)
+    {
+        rgb>255;
+        rgb=255
+
+    }
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-    image[i][j].rgbtRed=round(image[i][j].rgbtRed*0.393+image[i][j].rgbtGreen*0.769+image[i][j].rgbtBlue*0.189);
-   image[i][j].rgbtGreen =round(image[i][j].rgbtRed*0.349+image[i][j].rgbtGreen*0.686+image[i][j].rgbtBlue*0.168);
-   image[i][j].rgbtBlue=round(image[i][j].rgbtRed*0.272+image[i][j].rgbtGreen*0.534+image[i][j].rgbtBlue*0.131);
-    if (image[i][j].rgbtRed>255)
-{
-    image[i][j].rgbtRed=255;
-}
-if (image[i][j].rgbtGreen>255)
-{
-     image[i][j].rgbtGreen=255;
-}
-if (image[i][j].rgbtBlue>255)
-{
-     image[i][j].rgbtBlue=255;
-}
-else
-image[i][j].rgbtGreen=image[i][j].rgbtGreen;
-image[i][j].rgbtBlue=image[i][j].rgbtBlue;
-image[i][j].rgbtRed=image[i][j].rgbtRed;
-
+    image[i][j].rgbtRed=limit(round(image[i][j].rgbtRed*0.393+image[i][j].rgbtGreen*0.769+image[i][j].rgbtBlue*0.189));
+   image[i][j].rgbtGreen =limit(round(image[i][j].rgbtRed*0.349+image[i][j].rgbtGreen*0.686+image[i][j].rgbtBlue*0.168));
+   image[i][j].rgbtBlue=limit(round(image[i][j].rgbtRed*0.272+image[i][j].rgbtGreen*0.534+image[i][j].rgbtBlue*0.131));
 
 
     return;
