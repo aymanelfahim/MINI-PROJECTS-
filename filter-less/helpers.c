a4635b456651c8b8574c9e20ec72af8af46fb7d1
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-21 12:50:02 +0000

    Thu, Jul 21, 2022, 12:50 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -50,16 +50,16 @@ return;
 
 // Reflect image horizontally
 
-void swap(int a, int b);
+void swap(int *a, int *b);
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
       for (int i = 0; i < height; i++)
       {
           for (int j = 0; j < round(width*0.5); j++)
           {
-            swap(&image[i][j].rgbtBlue,&image[i][width-j].rgbtBlue);
-             swap(&image[i][j].rgbtRed,&image[i][width-j].rgbtRed);
-              swap(&image[i][j].rgbtGreen,&image[i][width-j].rgbtGreen);
+            swap(image[i][j].rgbtBlue, image[i][width-j].rgbtBlue);
+             swap(image[i][j].rgbtRed,image[i][width-j].rgbtRed);
+              swap(image[i][j].rgbtGreen,image[i][width-j].rgbtGreen);
 
 
           }
