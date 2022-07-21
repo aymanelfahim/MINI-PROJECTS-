f4faba17621867455b6b2774968c1ed07eaee249
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-21 16:19:12 +0000

    Thu, Jul 21, 2022, 4:19 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -75,5 +75,12 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+     for (int i = 0; i < height; i++)
+     {
+         for (int j = 0; j < height; j++)
+         {
+            
+         }
+     }
     return;
 }
