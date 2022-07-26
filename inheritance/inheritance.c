bf9edf9de2dc2ecdd3bd8e91267e93a74ee24a9f
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-26 17:50:32 +0000

    Tue, Jul 26, 2022, 5:50 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -45,8 +45,8 @@ person *create_family(int generations)
     if (generations > 1)
     {
         // Create two new parents for current person by recursively calling create_family
-        person *parent[0] = create_family(generations - 1);
-        person *parent[1] = create_family(generations - 1);
+        new->parent[0] = create_family(generations - 1);
+        new ->parent[1] = create_family(generations - 1);
         new->alleles[0]=random_allele(new->parents[0]);
         new->alleles[1]=random_allele(new->parents[1]);
 
