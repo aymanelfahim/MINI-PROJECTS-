67cbc341f7217896669c61223a5e2d842d16b57f
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-26 13:31:29 +0000

    Tue, Jul 26, 2022, 1:31 PM +00:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -39,7 +39,7 @@ int main(void)
 // Create a new individual with `generations`
 person *create_family(int generations)
 {
-    person*
+    person*tree= NULL;
 
     // If there are still generations left to create
     if (generations > 1)
