81ba21d7f714e48c56b742d323275d90372ad7af
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2022-07-28 12:27:11 +0000

    Thu, Jul 28, 2022, 12:27 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -29,6 +29,7 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
+    word
     // TODO: Improve this hash function
 
     return toupper(word[0]) - 65;
