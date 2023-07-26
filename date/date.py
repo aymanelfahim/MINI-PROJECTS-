4e5fe5f1992a60f4369de3251841b1637c073fe8
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2023-07-26 10:43:55 +0000

    ddd, Jul 7/26/2023, YYYY, 10:43 A +0

--- date/date.py
+++ date/date.py
@@ -1,5 +1,19 @@
+months=[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
 while True:
     try:
-        date=input("Date:").split("/")
+        dat
         if input
 
