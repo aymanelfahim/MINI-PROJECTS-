79d233a6ed3a89c2a827dcf2c22f173a0a9ddaf6
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2023-07-26 10:32:47 +0000

    ddd, Jul 7/26/2023, YYYY, 10:32 A +0

--- date/date.py
+++ date/date.py
@@ -1,4 +1,5 @@
 while True:
-    date=input("Date:").split("/")
     try:
-        month,day,year=int(date[0]),int(date[1]),int(date[2]) or 
+        date=input("Date:").split("/")
+        if input
+
