31a3c75e4f8a8c18d6b19384be10b8eb250747c2
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2023-07-26 11:03:10 +0000

    ddd, Jul 7/26/2023, YYYY, 11:03 A +0

--- date/date.py
+++ date/date.py
@@ -17,11 +17,12 @@ while True:
     try:
         date=input("date")
         if "/" in date:
-            L=date.split(/)
+            L=date.split("/")
             print(L[1]+"/"+L[0]+"/"+L[2])
         else:
             M=date.replace(",",("")).split()
-            print(M.index())
+            print(M[1]+"/"+months.index(M[0])+"/"+M[2])
 
-
+    except KeyError:
+        pass
 
