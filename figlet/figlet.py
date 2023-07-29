f970b53b7eb268a68eab3213f31de4975905f653
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2023-07-29 12:40:48 +0000

    ddd, Jul 7/29/2023, YYYY, 12:40 A +0

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,10 +4,8 @@ def main():
     if len(sys.argv)==3:
         if sys.argv[1]== "-f" or sys.argv[2]== "--font":
             inp=input("input: ")
-            try:
-                print("output: " ,pfg.figlet_format(inp,font=sys.argv[2]))
-            except FontNotFound:
-                print ("invalid kda")
+            print("output: " ,pfg.figlet_format(inp,font=sys.argv[2]))
+
         else:
             sys.exit("faute technique ")
     elif len(sys.argv)==1:
