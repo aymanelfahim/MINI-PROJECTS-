26a856f29f6725b8aff04fc7bee548cdd713ff4e
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2023-07-29 12:07:38 +0000

    ddd, Jul 7/29/2023, YYYY, 12:07 A +0

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,7 +1,12 @@
 import pyfiglet as pfg
 import sys
 def main():
-    if len(sys.argv)=
+    if len(sys.argv)==3:
+        inp=input("input: ")
+        print("output: " ,pfg.figlet_format(inp,font=sys.argv[2]))
+    elif len(sys.argv)=
+
+
 
 
 
