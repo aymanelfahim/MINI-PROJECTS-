b48bbd7b0d4278402707d80ead2644fa2112ec8f
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2023-07-31 18:59:51 +0000

    ddd, Jul 7/31/2023, YYYY, 6:59 A +0

--- tjriba/tjriba.py
+++ tjriba/tjriba.py
@@ -6,7 +6,7 @@ def mhi(s):
     for i in range(1,len(L)-1):
         for j in range(i):
                 if L[j]==L[i]:
-                    del(L[j])
+                    del(L[i])
 
     return L
 n=input("donner :")
