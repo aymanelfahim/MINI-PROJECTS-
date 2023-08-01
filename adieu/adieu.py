a37add21d446da9a51bad0c9234ad30924897687
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2023-08-01 13:13:57 +0000

    ddd, Aug 8/1/2023, YYYY, 1:13 A +0

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,18 +1,16 @@
 import inflect
-def main():
-    S=""
-    while True:
-        try:
-            n=input("Name: ")
-        except EOFError:
-            print("adieu,adieu to ",inflect.join(M))
-            break
-        else:
-            S=S+","+n
+S=""
+while True:
+    try:
+        n=input("Name: ")
+    except EOFError:
+        print("adieu,adieu to ",inflect.join(M))
+        break
+    else:
+        S=S+","+n
         M=S.split(",")
 
 
 
 
-main()
 
