09f494b1dd62b5b369ae9be7da00dd9137dc3e60
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2023-08-01 13:10:52 +0000

    ddd, Aug 8/1/2023, YYYY, 1:10 A +0

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,16 +1,16 @@
-import inflect
+import inflect as p
 def main():
     S=""
     while True:
         try:
             n=input("Name: ")
         except EOFError:
-            print("adieu,adieu to ",S)
+            print("adieu,adieu to ",p.join(M))
             break
         else:
             S=S+","+n
         M=S.split(",")
-        M=
+
 
 
 
