6e56e5a35c62d8afede0f94c2c903a41e67298fc
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2023-08-02 12:25:43 +0000

    ddd, Aug 8/2/2023, YYYY, 12:25 A +0

--- game/game.py
+++ game/game.py
@@ -15,12 +15,13 @@ def main()
  def get(n):
     while True:
       n=int(input("doner n: "))
-
-
-
-
-
-
-
-
+      if n>0:
+        break
+    return n
 def guess(k):
+    a=random.randrange(1, n)
+    while True:
+        k=int(input("donner un nombre:"))
+        if k>0:
+           
+
