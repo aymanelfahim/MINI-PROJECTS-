f5bc6a2d5130bef7277b8ba18266cce57d7ff58e
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2023-08-06 11:57:24 +0000

    ddd, Aug 8/6/2023, YYYY, 11:57 A +0

--- line/line.py
+++ line/line.py
@@ -1,10 +1,12 @@
 import sys
-if len(sys.argv)>2:
-    print("too much command lines")
-elif len(sys.argv)<2:
-    print("too few command lines")
-else:
-    if "py" not in sys.argv[1]:
-        print("not a python file")
+def main()
+    getparametre()
+def getparametre()
+    if len(sys.argv)>2:
+        print("too much command lines")
+    elif len(sys.argv)<2:
+        print("too few command lines")
     else:
-        
+        if "py" not in sys.argv[1]:
+            print("not a python file")
+        else:
