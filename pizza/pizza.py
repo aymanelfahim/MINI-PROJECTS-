6251d9ff76093092ce180468b68449049ebea11b
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2023-08-06 16:41:15 +0000

    ddd, Aug 8/6/2023, YYYY, 4:41 A +0

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,2 +1,18 @@
 def main():
-    
+
+
+
+
+
+
+
+def getparametre(K):
+    if len(K)>2:
+        sysexit("too much command lines")
+    elif len(K)<2:
+        sys.exit("too few command lines")
+    else:
+        if  not in K[1]:
+            sys.exit("not a python file")
+
+    return True
