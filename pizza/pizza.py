e1e39478b22abb43454261ac40a122551bc2d7fc
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2023-08-07 10:50:17 +0000

    ddd, Aug 8/7/2023, YYYY, 10:50 A +0

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -2,13 +2,15 @@ import sys
 import tabulate
 import csv
 def main():
-    L=[]
-    with open(sys.argv[1]) as file:
-        reader = csv.reader(file)
-        for row in reader:
-            L=L.append(row)
+    if getparametre(sys.argv[1]):
 
-    print (tabulate(L,headers,tablefmt="grid"))
+        L=[]
+        with open(sys.argv[1]) as file:
+            reader = csv.reader(file)
+            for row in reader:
+                L=L.append(row)
+
+        print (tabulate(L,headers,tablefmt="grid"))
 
 
 
@@ -19,7 +21,7 @@ def main():
 
 def getparametre(K):
     if len(K)>2:
-        sysexit("too much command lines")
+        sys.exit("too much command lines")
     elif len(K)<2:
         sys.exit("too few command lines")
     else:
