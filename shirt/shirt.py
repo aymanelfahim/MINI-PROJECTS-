c6acc99577ca86a91b079ce74ef332203c204208
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2023-08-07 13:27:32 +0000

    ddd, Aug 8/7/2023, YYYY, 1:27 A +0

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -26,7 +26,7 @@ def getimage(L):
     elif len(L)>3:
         sys.exit("too much command line arguments")
     else:
-        if "jpg" or "jpeg" or "png" not in L:
+        if "jpg" or "jpeg" or "png" not in L[1] or L[2]:
             sys.exit("not the format")
         elif
 
@@ -35,5 +35,11 @@ def getimage(L):
 
 
 def rech(a,b,c,L):
-    
+    if a in L[1] and in L[2]:
+        return a
+    elif b in L[1]:
+        return b
+    elif c in L[1]:
+    return c
+
 
