65ca8d1d6c515b3256e1f669988e03d794082475
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2023-08-15 20:04:24 +0000

    ddd, Aug 8/15/2023, YYYY, 8:04 A +0

---
+++ test/test.py
@@ -0,0 +1,8 @@
+def egale(A,B):
+    if len(A)!=len(B):
+        return False
+    elif A[0]!=B[0]:
+        return False
+    else:
+        return egale(A[1:],B[1:])
+print(egale([1,2,3],[1,2,3]))
