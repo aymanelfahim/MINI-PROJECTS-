285b120a8a86d68df227da707bb3f3200d4f51f0
aymanelfahim <108943843+aymanelfahim@users.noreply.github.com>
2023-08-15 20:12:41 +0000

    ddd, Aug 8/15/2023, YYYY, 8:12 A +0

--- test/test.py
+++ test/test.py
@@ -3,6 +3,8 @@ def egale(A,B):
         return False
     elif A[0]!=B[0]:
         return False
+    elif A==[]:
+        return True
     else:
         return egale(A[1:],B[1:])
 print(egale([1,2,3,4],[1,2,3,4]))
